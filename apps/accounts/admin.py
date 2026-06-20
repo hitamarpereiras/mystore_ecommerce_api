6151c6a8c622b7759b6b8e084e01ac6fe8ec99ee
hitamarpereiras <hitamarsilvaoficial@gmail.com>
2026-06-20 19:03:36 -0300

    3º atualização no app/accounts

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from apps.accounts.models import User
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'email',
+        'first_name',
+        'last_name',
+        'is_active',
+        'telephone'
+    )
+
+    list_filter = (
+        'is_active',
+    )
+    search_fields = (
+        'email',
+        'first_name',
+        'telephone'
+    )
