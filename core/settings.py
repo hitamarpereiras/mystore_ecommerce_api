9614e7abd8bfaa4b9dbff739f08e1ccd1c3c6649
hitamarpereiras <hitamarsilvaoficial@gmail.com>
2026-06-21 10:30:24 -0300

    implementação de autenticação de usuarios

--- core/settings.py
+++ core/settings.py
@@ -31,7 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
-    'rest_framework.authtoken',
+    'rest_framework_simplejwt',
     'django_filters',
 
     'apps.accounts',
--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('authentication.urls')),
     path('api/v1/', include('apps.stores.urls')),
     path('api/v1/', include('apps.accounts.urls')),
 ]
