3c597825cbc3140c60a69f54ce8d96c04ff6e4e0
hitamarpereiras <hitamarsilvaoficial@gmail.com>
2026-06-24 18:00:06 -0300

    admin atualizado no app/orders

--- apps/orders/admin.py
+++ apps/orders/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from apps.orders.models import Order
 
 # Register your models here.
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = [
+        'customer',
+        'store',
+        'name_customer',
+        'phone',
+        'address',
+        'total',
+        'status',
+        'created_at',
+    ]
+    search_fields = [
+        'phone',
+        'created_at',
+    ]
