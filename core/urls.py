f1471a00a4bb76a7cfd6fbb7406f0145a3af7d64
hitamarpereiras <hitamarsilvaoficial@gmail.com>
2026-06-24 23:18:45 -0300

    urls padronizadas em core

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('apps.authentication.urls')),
+    path('api/v1/', include('apps.authentication.urls')),
     path('api/v1/', include('apps.accounts.urls')),
     path('api/v1/', include('apps.customers.urls')),
     path('api/v1/', include('apps.stores.urls')),
